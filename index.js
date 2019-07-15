e0ae0fac249286350cc89b9794627c0f437dd543
Guilherme Oliveira <htoguilherme@gmail.com>
2019-07-15 16:22:47 -0300

    Allowed cross origin requestes

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const app = require("express")();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
-io.on('connection', socket => {
-    socket.join(socket.handshake.query.roomId)
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-    socket.on('join room', roomId => {
-        io.emit('player joined', roomId)
-        socket.join(roomId)
-    })
+io.on("connection", socket => {
+    socket.join(socket.handshake.query.roomId);
 
-    socket.on('play', gameState => {
-        socket.to(gameState.roomId).emit('play', gameState)
-    })
-})
+    socket.on("join room", roomId => {
+        io.emit("player joined", roomId);
+        socket.join(roomId);
+    });
+
+    socket.on("play", gameState => {
+        socket.to(gameState.roomId).emit("play", gameState);
+    });
+});
 
 http.listen(3000, () => {
-    console.log('Express server lisetning on port 3000')
-})
+    console.log("Express server lisetning on port 3000");
+});
