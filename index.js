7941edc07a39894533cc564df3b17af9b6bcf7bb
Guilherme Oliveira <htoguilherme@gmail.com>
2019-07-15 16:36:45 -0300

    Added process port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ io.on("connection", socket => {
     });
 });
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log("Express server lisetning on port 3000");
 });
